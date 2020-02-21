50dc5950c1a7d8553b48c33b3ea79038b7b750d1
Fredrik Olsen Skogvik <foskog.dev@gmail.com>
2020-02-21 09:20:09 +0100

    Initial Commit

---
+++ PG5600_Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PG5600_Exam
+//
+//  Created by Fredrik Skogvik on 21/02/2020.
+//  Copyright © 2020 Fredrik Skogvik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
