5d36800162e6eb0f0612adab5a8d20095d48e5ef
Fredrik Olsen Skogvik <foskog.dev@gmail.com>
2020-03-27 11:24:17 +0100

    Moved New image selection functionality from gallery controller to image selection controller. Starting to work on passing it further down the pipeline.

--- PG5600_Exam/Controller/ImageSelectCollectionViewController.swift
+++ PG5600_Exam/Controller/ImageSelectCollectionViewController.swift
@@ -7,102 +7,108 @@
 //
 
 import Photos
+import PhotosUI
 import UIKit
 
 private let reuseIdentifier = "ImageCell"
 
 class ImageSelectCollectionViewController: UICollectionViewController {
-    @IBOutlet var submitButton: UIBarButtonItem!
+    var fetchResult: PHFetchResult<PHAsset>!
+       let imageManager = PHCachingImageManager()
+       var assetCollection: PHAssetCollection!
+
+       var availableWidth: CGFloat = 0
+       fileprivate var thumbnailSize: CGSize!
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
-    //let imageManager = ImageManager()
+       override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Select Images"
+           // Determine the size of the thumbnails to request from the PHCachingImageManager.
+           let scale = UIScreen.main.scale
+           let cellSize = flowLayout.itemSize
 
-        // makes selecting multiple images in the view possible.
-        collectionView.allowsMultipleSelection = true
+           // This bases itself on the size of the cell. EG. this line states that images fetched by the manager should have the exact same size as the cells.
+           thumbnailSize = CGSize(width: cellSize.width * scale, height: cellSize.height * scale)
+       }
 
-        // call loadimage function to get all photos in the gallery.
-        //imageManager.loadImage()
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
-        clearsSelectionOnViewWillAppear = true
+           collectionView.allowsMultipleSelection = true
+           
+           let allPhotosOptions = PHFetchOptions()
+           // sets sorting order of fetched images, sorted ascending by creation date.
+           allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+           // retrieves all assets that matches our options and assigns them to the fetchResult collection.
+           fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
+       }
+
+       override func viewWillLayoutSubviews() {
+           super.viewWillLayoutSubviews()
+           let width = view.bounds.inset(by: view.safeAreaInsets).width
+           // Adjust the item size if the available width has changed.
+           if availableWidth != width {
+               availableWidth = width
+               // increasing the "divided by" number decreases the number of cells in the column
+               // these calculate the size of the flowlayout cells based on the size of the phone. Which is why the images are scaled correctly based on which device. Dont think they're related to image or content of the cell.
+               let columnCount = (availableWidth / 140).rounded(.towardZero)
+               let itemLength = (availableWidth - columnCount - 12) / columnCount
+
+               flowLayout.itemSize = CGSize(width: itemLength, height: itemLength)
+           }
+       }
+
+       /*
+        // MARK: - Navigation
+
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-            submitButton.isEnabled = false
-        
-    }
+        }
+        */
 
-    
-     // MARK: - Navigation
+       // MARK: UICollectionViewDataSource
 
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-          if segue.identifier == "presentModal" {
-            let destinationVC = segue.destination as! ImageSelectModalController
-            //destinationVC.selectedImages = imageManager.selectedImageArray
-     }
-    }
-     
+       override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return fetchResult.count
+       }
 
-    // MARK: UICollectionViewDataSource
+       override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let asset = fetchResult.object(at: indexPath.item)
+           // research implementation better.
+           guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ImageCell
+           else { fatalError("Unexpected cell in collection view") }
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
-        // #warning Incomplete implementation, return the number of items
-        //return imageManager.imageArray.count
-    }
+           // Request an image for the asset from the PHCachingImageManager.
+           imageManager.requestImage(for: asset, targetSize: flowLayout.itemSize, contentMode: .aspectFill
+                                     , options: nil) { image, _ in
+               cell.thumbnailImage = image
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // Displays all the cells in the collection
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCell
+               // Format date. don't think this is the best place for the functionality. Will look into moving it.
+               let formatter = DateFormatter()
+               formatter.locale = Locale(identifier: "no_NO")
+               formatter.timeStyle = .short
+               formatter.dateStyle = .short
+               cell.timeStampLabel.text = formatter.string(from: asset.creationDate!)
+           }
+           return cell
+       }
 
-        // Gets a reference to the cell in the storyboard
-        let imageView = cell.viewWithTag(1) as! UIImageView
-        // Gets a reference to the titleLabel in the storyboard
-        //let label = cell.imageLabel!
+       // MARK: UICollectionViewDelegate
 
-        // sets the image in the view to the first one from the imagearray
-        //imageView.image = imageManager.imageArray[indexPath.row]
-        // sets the text in the label to the date taken from the datearray and converts it to a sting
-//        label.text = imageManager.dateArray[indexPath.row].toString(dateFormat: "yyyy/MMM/dd")
 
-        print(indexPath.row)
-
-        // return the cell
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCell
-//        let image = imageManager.imageArray[indexPath.row]
-//
-//        if imageManager.selectedImageArray.contains(image) == false {
-//            cell.isSelected = true
-//            imageManager.selectedImageArray.insert(image, at: indexPath.row)
-//        }
-//
-//        if imageManager.selectedImageArray.isEmpty {
-//            submitButton.isEnabled = false
-//        } else {
-//            submitButton.isEnabled = true
-//        }
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCell
-        var index = 0
-//        for image in imageManager.selectedImageArray {
-        index = indexPath.row
-//        }
-//        imageManager.selectedImageArray.remove(at: index)
-//        cell.isSelected = false
-//
-//        if imageManager.selectedImageArray.isEmpty {
-//            submitButton.isEnabled = false
-//        } else {
-//            submitButton.isEnabled = true
-//        }
-        
-    }
+        override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ImageCell
+           else { fatalError("Unexpected cell in collection view") }
+           
+           print(cell.isSelected)
+       }
+       
+        override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+           guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? ImageCell
+           else { fatalError("Unexpected cell in collection view") }
+           
+           print(cell.isSelected)
+       }
 }
